449bd618398945321ebe75dca32aab7b48b5449f
Thu Vo <votranminhthu00@gmail.com>
2020-07-22 18:19:45 -0400

    Update index.js
    Modify index.js to add support for FCM

--- index.js
+++ index.js
@@ -11,7 +11,12 @@ if (!databaseUri) {
   console.log('DATABASE_URI not specified, falling back to localhost.');
 }
 
+if (process.env.FCM_API_KEY) {
+    pushConfig['android'] = { apiKey: process.env.FCM_API_KEY || ''};
+}
+
 var api = new ParseServer({
+  push: pushConfig,
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'myAppId',
