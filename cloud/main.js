5e082b86deed98a66e90e6546bcdaa8280675712
Thu Vo <votranminhthu00@gmail.com>
2020-07-27 17:46:57 -0400

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -1,12 +1,12 @@
 Parse.Cloud.define('hello', function(req, res) {
 	var pushQuery = new Parse.Query(Parse.Installation);
 // pushQuery.containedIn("user", userlist);
-return Parse.Push.send({
-	where: pushQuery, 
-	data: {
-		alert: "Your push message here!"
-	}
-}, {
+
+	return Parse.Push.send({
+		where: pushQuery, 
+		data: { alert: "Your push message here!"}
+	}, { useMasterKey: true }, 
+	{
 	success: function() {
 		response.success("pushed");
 	}, error: function(error) {
