2372d0c4a3953aee2ab921efb932aad3c5d8ed9c
Thu Vo <votranminhthu00@gmail.com>
2020-07-27 17:32:36 -0400

    Update main.js
    test push

--- cloud/main.js
+++ cloud/main.js
@@ -1,3 +1,18 @@
 Parse.Cloud.define('hello', function(req, res) {
-  return 'Hi Foodie!';
+	var pushQuery = new Parse.Query(Parse.Installation);
+// pushQuery.containedIn("user", userlist);
+Parse.Push.send({
+	where: pushQuery, 
+	data: {
+		alert: "Your push message here!"
+	}
+}, {
+	success: function() {
+		response.success("pushed");
+	}, error: function(error) {
+		reponse.error("didn't push");
+	}
+});
+
+return 'Hi Foodie!';
 });
