46749ea87bd98f5b66c5b67d634cb9b0e6df93a6
Thu Vo <votranminhthu00@gmail.com>
2020-07-28 11:47:00 -0400

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -2,9 +2,9 @@ Parse.Cloud.define('hello', function(req, res) {
 	var user = req.user;
 	var params = req.params;
 
-	const otherUserQuery = new Parse.Query(Parse.User);
+	/*const otherUserQuery = new Parse.Query(Parse.User);
 	otherUserQuery.equalTo("objectId", params.otherUser);  
-	const otheruser = await otherUserQuery.find();
+	const otheruser = await otherUserQuery.find();*/
 
 
     var pushQuery = new Parse.Query(Parse.Installation);
@@ -13,7 +13,7 @@ Parse.Cloud.define('hello', function(req, res) {
     return Parse.Push.send({
         where: pushQuery,
         data: {
-            alert: "message from " + user.get("username") + " to" + otheruser.get("username")
+            alert: "message from " + user.get("username") + " to" + //otheruser.get("username")
         }
     }, {
         useMasterKey: true
