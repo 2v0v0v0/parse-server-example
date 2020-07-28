772936f54c61a52e5d2edd45c55a6a6ca327a10a
Thu Vo <votranminhthu00@gmail.com>
2020-07-28 12:08:59 -0400

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -2,9 +2,9 @@ Parse.Cloud.define('hello', function(req, res) {
 	var user = req.user;
 	var params = req.params;
 
-	const query = new Parse.Query(Parse.User);
+	/*var query = new Parse.Query(Parse.User);
 	query.equalTo("username", "user2");  // find all the women
-	const women = await query.find();
+	var women = await query.find();*/
 
     var pushQuery = new Parse.Query(Parse.Installation);
     pushQuery.equalTo("user",user);
